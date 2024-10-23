939947d777e491b6e0cdc0f61580f0d225a1b427
student <student@WS16-06>
2024-10-23 13:41:11 +0400

    1-commit

---
+++ ychet/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace ychet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(158, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Количество";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Название";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 123);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(23, 162);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(114, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(291, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(458, 531);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(57, 303);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(201, 80);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Посмотреть данные";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(23, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(250, 21);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Выбрать кабинет";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 575);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ychet/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Drawing.Text;
+
+namespace ychet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();      
+        }
+
+        DB db = new DB();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            db.getConnection();
+            SQLiteConnection connection = new SQLiteConnection(db.connection);
+            connection.Open();  
+            var command = new SQLiteCommand("SELECT id, name FROM location", connection);
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                comboBox1.Items.Add(new { Text = reader["name"].ToString(), Value = reader["id"] });
+            }
+            comboBox1.DisplayMember = "Text";
+            comboBox1.ValueMember = "Value";
+
+            SetupDataGridView();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try { 
+                db.getConnection();
+
+            if (comboBox1.Text != string.Empty)
+            {
+                var selectedRoom = (dynamic)comboBox1.SelectedItem;
+                long roomID = selectedRoom.Value;
+
+                dataGridView1.Rows.Clear();
+
+                SQLiteConnection connection = new SQLiteConnection(db.connection);
+                connection.Open();
+
+                var command = new SQLiteCommand("SELECT name, quantity FROM technika WHERE location_id = @roomID", connection);
+                command.Parameters.AddWithValue("@roomID", roomID);
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(reader["name"].ToString(),
+                                           reader["quantity"].ToString());
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выбирите кабинет");
+            }
+        }
+            catch
+            {
+                MessageBox.Show("Выбирите кабинет");
+            }
+        }
+
+
+        private void SetupDataGridView()
+        {
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+
+            dataGridView1.ColumnCount = 2;
+
+            dataGridView1.Columns[0].Name = "Наименование";
+            dataGridView1.Columns[1].Name = "Количество";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != string.Empty
+               && textBox2.Text != string.Empty)
+            {
+                db.getConnection();
+                SQLiteConnection connection = new SQLiteConnection(db.connection);        
+                connection.Open();
+
+                var selectedRoom = (dynamic)comboBox1.SelectedItem;
+                long roomId = selectedRoom.Value;
+                string Name = textBox1.Text;
+                string quant = textBox2.Text;
+
+                var command = new SQLiteCommand("INSERT INTO technika (name, quantity, location_id) VALUES (@name, @quant, @locationId)", connection);
+                command.Parameters.AddWithValue("@name", Name);
+                command.Parameters.AddWithValue("@quant", quant);
+                command.Parameters.AddWithValue("@locationId", roomId);
+
+                command.ExecuteNonQuery();
+
+                LoadTech(roomId);
+            }
+            else
+            {
+                MessageBox.Show("Заполните данные");
+            }
+        }
+
+        void LoadTech(long roomId)
+        {
+            dataGridView1.Rows.Clear();
+
+
+            var selectedRoom = (dynamic)comboBox1.SelectedItem;
+            long roomID = selectedRoom.Value;
+
+            dataGridView1.Rows.Clear();
+
+            SQLiteConnection connection = new SQLiteConnection(db.connection);
+            connection.Open();
+
+            var command = new SQLiteCommand("SELECT name, quantity FROM technika WHERE location_id = @roomID", connection);
+            command.Parameters.AddWithValue("@roomID", roomID);
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                dataGridView1.Rows.Add(reader["name"].ToString(),
+                                       reader["quantity"].ToString());
+
+            }
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 1)
+            {
+                var selectedRoom = (dynamic)comboBox1.SelectedItem;
+
+                long roomId = selectedRoom.Value;
+
+                int rowIndex = e.RowIndex;
+
+                string Name = dataGridView1.Rows[rowIndex].Cells[0].Value.ToString();
+
+                int newQuant = Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells[1].Value);
+
+                db.getConnection();
+                SQLiteConnection connection = new SQLiteConnection(db.connection);
+                connection.Open();
+                var command = new SQLiteCommand("UPDATE technika SET quantity = @newQuant WHERE name = @Name AND location_id = @roomId", connection);
+
+                command.Parameters.AddWithValue("@newQuant", newQuant);
+                command.Parameters.AddWithValue("@Name", Name);
+                command.Parameters.AddWithValue("@roomId", roomId);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedItem != null && dataGridView1.SelectedRows.Count > 0)
+            {
+                db.getConnection();
+                SQLiteConnection connection = new SQLiteConnection(db.connection);
+                connection.Open();
+
+                var selectedRoom = (dynamic)comboBox1.SelectedItem;
+                long roomId = selectedRoom.Value;
+                int selectedRow = dataGridView1.SelectedRows[0].Index;
+                string Name = dataGridView1.Rows[selectedRow].Cells[0].Value.ToString();
+
+                var command = new SQLiteCommand("DELETE FROM technika WHERE name = @name AND location_id = @roomId", connection);
+                command.Parameters.AddWithValue("@name", Name);
+                command.Parameters.AddWithValue("@roomId", roomId);
+
+                int rowsAff = command.ExecuteNonQuery();
+                if (rowsAff > 0)
+                {
+                    MessageBox.Show("Успешно удалено");
+                    dataGridView1.Rows.RemoveAt(selectedRow);
+                }
+                else
+                {
+                    MessageBox.Show("Не удалось удалить технику");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите кабинет и технику для удаления");
+            }
+        }
+    }
+}
